53aa808f7e3cf782b07a413532bf64906d9f2c7f
AndyAnguiano <aanguiano@chapman.edu>
2021-03-19 09:49:50 -0700

    start over

--- data_ingestion.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('./StudentDB.sqlite')
-mycursor = conn.cursor()
-
-#import data from csv
-with open(./students.csv) as inputfile:
-    columns = inputfile.readline()
-    rows = inputfile.readline()
